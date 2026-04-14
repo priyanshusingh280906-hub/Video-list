0498ff66e3b8e1a239fb6779fcc371cf7c1d0bc4
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-04-14 13:02:48 +0530

    added a add video button in the webpage

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.body.style.backgroundColor = "black";
+document.body.style.backgroundColor = "lightgray";
 
 function cloneVideoList() {
 
 let originalVideoList = document.querySelector('.video-list');
 let videoList = originalVideoList.cloneNode(true);
 
+let videotitle = videoList.querySelector('.video-info h2');
+videotitle.textContent = "Cloned Video Title";
+
 document.querySelector(".container").appendChild(videoList);
 }
 
-cloneVideoList();
-cloneVideoList();
