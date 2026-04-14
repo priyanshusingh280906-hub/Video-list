e082070d86eea9ff11331bf97e0aa3382c36ec1f
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-04-14 13:12:19 +0530

    added some function

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ let originalVideoList = document.querySelector('.video-list');
 let videoList = originalVideoList.cloneNode(true);
 
 let videotitle = videoList.querySelector('.video-info h2');
-videotitle.textContent = "Cloned Video Title";
+videotitle.textContent = prompt("Enter the video title:");
+
+let videoviews = videoList.querySelector('.views');
+videoviews.textContent = prompt("Enter the number of views:") + " views";
+
+let videodate = videoList.querySelector('.date');
+videodate.textContent = prompt("Enter the days ago:") + " days ago";
 
 document.querySelector(".container").appendChild(videoList);
 }
 
+document.querySelector(".add-video").addEventListener("click", cloneVideoList);
